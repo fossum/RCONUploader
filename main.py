1701d22c74fbbcc0189fa78de7c254a587cf0d8e
Eric Fossum <fossum.eric@gmail.com>
2024-02-01 00:58:13 +0000

    Fully working RCON query

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
 
+from os import getenv
 from rcon.source import Client
 
-with Client(host, rcon_port, passwd=password) as client:
+host = getenv('HOST', 'localhost')
+port = int(getenv('PORT', 25575))
+password = getenv('PASSWORD')
+
+with Client(host, port, passwd=password) as client:
     print(client.run('ShowPlayers', enforce_id=False))
     # print(client.run('Save', enforce_id=False))
